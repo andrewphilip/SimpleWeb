26a0823e0b8d20114c246581913060d378452c72
andrewphilip <andrewphilip@users.noreply.github.com>
2018-02-11 09:56:19 -0500

    modified mainclass for war deployment

--- src/main/java/andy/web/SimpleWebApplication.java
+++ src/main/java/andy/web/SimpleWebApplication.java
@@ -5,14 +5,21 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SimpleWebApplication {
+public class SimpleWebApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SimpleWebApplication.class);
 	}
 	
 	@Bean
